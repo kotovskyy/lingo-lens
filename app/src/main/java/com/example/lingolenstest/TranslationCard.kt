71e2b8fe71c3d4a3375ef21da8175b0f1c59c66e
acodered <alexkondratru@gmail.com>
2024-10-08 15:56:44 +0200

    Add voice pronunciation of translated words

--- app/src/main/java/com/example/lingolenstest/TranslationCard.kt
+++ app/src/main/java/com/example/lingolenstest/TranslationCard.kt
@@ -2,12 +2,22 @@ package com.example.lingolenstest
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
@@ -17,6 +27,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,11 +37,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lingolenstest.translateAPI.DefinitionDetail
 import com.example.lingolenstest.translateAPI.TranslationResponse
 import com.example.lingolenstest.translateAPI.TranslatorInstance
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @Composable
 fun TranslationCard(
@@ -44,12 +59,30 @@ fun TranslationCard(
 
     val translationLanguage by remember { mutableStateOf(defaultTranslationLanguage) }
 
+    var tts by remember { mutableStateOf<TextToSpeech?>(null) }
+
+    LaunchedEffect(translationLanguage) {
+        tts = TextToSpeech(context) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                tts?.language = Locale(translationLanguage.code)
+            }
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            tts?.stop()
+            tts?.shutdown()
+        }
+    }
+
     var translation by remember { mutableStateOf("") }
     var transcription by remember { mutableStateOf("") }
     var wordDescription by remember { mutableStateOf("") }
 
 
     val coroutineScope = rememberCoroutineScope()
+
 
     LaunchedEffect(translationLanguage) {
         if (translationLanguage.code != "nan") {
@@ -87,6 +120,9 @@ fun TranslationCard(
             ){
                 LanguagePicker("Translate to:", defaultLanguage = defaultTranslationLanguage) { language ->
                     saveTranslationLanguage(context, language.code)
+
+                    tts?.language = Locale(language.code)
+
                     coroutineScope.launch {
                         val wordTranslationResponse: TranslationResponse? = translateWord(word, sourceLangCode, language.code)
                         wordTranslationResponse?.let { response ->
@@ -104,7 +140,7 @@ fun TranslationCard(
 
                 }
 
-                IconButton({ onClose() }){
+                IconButton(onClick = { onClose() }){
                     Icon(
                         imageVector = Icons.Filled.Close,
                         contentDescription = "Back"
@@ -112,15 +148,39 @@ fun TranslationCard(
                 }
             }
             if (translationLanguage.code != "nan"){
-                Text(text = "Word: $word", style = MaterialTheme.typography.titleSmall)
+                Text(text = "Word: $word", fontWeight = FontWeight.Bold, fontSize = 24.sp)
                 if (translation.isNotEmpty()){
-                    Text(text = "Translation: $translation", style = MaterialTheme.typography.bodyLarge)
-                }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Translation: $translation",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            modifier = Modifier.weight(1f) // This makes the text take up available space
+                        )
+                        Spacer(modifier = Modifier.width(10.dp)) // Add space between the text and button
+                        IconButton(
+                            onClick = {
+                                tts?.speak(translation, TextToSpeech.QUEUE_FLUSH, null, null)
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.play_sound),
+                                contentDescription = "play",
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    }
+
+                                    }
                 if (transcription.isNotEmpty()) {
-                    Text(text = "Transcription: $transcription", style = MaterialTheme.typography.bodyMedium)
+                    Spacer(modifier = Modifier.height(20.dp).fillMaxWidth())
+                    Text(text = "Transcription: $transcription", fontWeight = FontWeight.Normal, fontSize = 20.sp)
                 }
                 if (wordDescription.isNotEmpty()){
-                    Text(text = "Description: $wordDescription", style = MaterialTheme.typography.bodyMedium)
+                    Text(text = "Description: $wordDescription", fontWeight = FontWeight.Light, fontSize = 18.sp)
                 }
             }
         }
